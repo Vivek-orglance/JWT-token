8feed4d5a3389cd860451c42b731ac7a297afa00
Harikrushna Vanpariya <h.g.vanpariya@gmail.com>
2024-01-17 11:41:29 +0530

    comlete jwt token

---
+++ src/test/java/com/authtoken/authtoken/AuthtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authtoken.authtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthtokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
